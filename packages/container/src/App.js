7eac4a1412113fe73cc180ddc9efd15571958de6
sajjad <sajjad.t.dev@gmail.com>
2024-06-14 16:04:33 +0330

    first commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+  return (
+    <div>
+      <h2>HI</h2>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
